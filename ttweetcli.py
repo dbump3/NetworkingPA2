8f4cc002dacb1034d2f841ff9d145e6f02cc81ae
Stephanie Chang <chieng.chang@gmail.com>
2020-03-16 18:26:17 -0400

    add user commands

--- ttweetcli.py
+++ ttweetcli.py
@@ -1,11 +1,72 @@
 import socket
 import sys
-import re
 
 #
 # TCP Echo Client Template Source:
 # https://pymotw.com/3/socket/tcp.html
 #
+
+
+def tweet(input):
+  # Check if message is less than 150 characters
+  server_message = ""
+  try:
+    hashtag_start = input.find("#")
+    message_start = input.find("\"")
+    message_end = input.find("\"", message_start + 1, hashtag_start)
+    server_message = input[message_start + 1:message_end]
+  except:
+    server_message = ""
+  if (server_message is None or len(server_message) == 0):
+    error("message format illegal.")
+  elif (len(server_message) > 150):
+    error("message length illegal, connection refused.")
+
+  server_request = bytes('u' + server_message, "utf-8")
+
+  try:
+    # Send data
+    print('sending {!r}'.format(server_request))
+    sock.sendall(server_request)
+
+    # Look for the response
+    amount_received = 0
+    amount_expected = len(server_request)
+
+    while amount_received < amount_expected:
+      data = sock.recv(16)
+      amount_received += len(data)
+      print('received {!r}'.format(data))
+
+  finally:
+    print('upload successful')
+    print('closing socket')
+    sock.close()
+
+
+def subscribe(input):
+  print()
+
+
+def unsubscribe(input):
+  print()
+
+
+def timeline():
+  print()
+
+
+def getusers():
+  print()
+
+
+def gettweets(input):
+  print()
+
+
+def exitProg():
+  sys.exit()
+
 
 def error(message):
   print("\n" + message + ".\n")
@@ -26,6 +87,7 @@ server_username = sys.argv[3]
 if not re.match("^[A-Za-z0-9]*$", server_username):
   error("error: username has wrong format, connection refused.")
 
+
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -35,56 +97,52 @@ server_address = (server_ip, server_port)
 try: sock.connect(server_address)
 except: error("Argument Error: No server found with this <ServerIP> and <ServerPort>")
 
-# Upload
-if (server_mode == "-u"):
-  server_request = bytes('u' + server_message, "utf-8")
-
-  try:
-    # Send data
-    print('sending {!r}'.format(server_request))
-    sock.sendall(server_request)
-
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(server_request)
-
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print('received {!r}'.format(data))
-
-  finally:
-    print('upload successful')
-    print('closing socket')
-    sock.close()
+# Wait for user commands
+user_input = input()
+command = user_input.split()[0]
+if command == "tweet":
+  tweet(user_input)
+elif command == "subscribe":
+  subscribe(user_input)
+elif command == "unsubscribe":
+  unsubscribe(user_input)
+elif command == "timeline":
+  timeline()
+elif command == "getusers":
+  getusers()
+elif command == "gettweets":
+  gettweets(user_input)
+elif command == "exit":
+  exitProg()
+else:
+  error("illegal command.")
 
 # Download
-elif (server_mode == "-d"):
-  server_request = bytes('d', "utf-8")
+server_request = bytes('d', "utf-8")
 
-  try:
-    # Send data
-    print('sending {!r}'.format(server_request))
-    sock.sendall(server_request)
+try:
+  # Send data
+  print('sending {!r}'.format(server_request))
+  sock.sendall(server_request)
 
-    # Look for the response
-    amount_received = 0
-    amount_expected = len(server_request)
+  # Look for the response
+  amount_received = 0
+  amount_expected = len(server_request)
 
-    while amount_received < amount_expected:
-      data = sock.recv(16)
-      amount_received += len(data)
-      print('received {!r}'.format(data))
+  while amount_received < amount_expected:
+    data = sock.recv(16)
+    amount_received += len(data)
+    print('received {!r}'.format(data))
 
-    sock.shutdown(1)
+  sock.shutdown(1)
 
-    while True:
-      data = sock.recv(16)
-      print('received {!r}'.format(data))
-      server_message += '{!r}'.format(data)[2:-1]
-      if (not data):
-        print('no data from', server_address)
-        break
-  finally:
-    print('\nmessage: ' + server_message + '\n')
-    sock.close()
+  while True:
+    data = sock.recv(16)
+    print('received {!r}'.format(data))
+    server_message += '{!r}'.format(data)[2:-1]
+    if (not data):
+      print('no data from', server_address)
+      break
+finally:
+  print('\nmessage: ' + server_message + '\n')
+  sock.close()
